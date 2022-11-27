1ca55f431fc59e6b74df2ed1f096dba4c9aff0f8
user <user@DESKTOP-1RNC1IK>
2022-11-27 20:05:24 +0200

    Add project files.

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Api.Controllers
+{
+	[Route("identity")]
+	[Authorize]
+	public class IdentityController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new JsonResult(User.Claims.Select(x => new { x.Type, x.Value }));
+		}
+	}
+}
